39b759524e240f357f7366317652e09b0860f14e
Ivan Urra <ivan.urra.wd@gmail.com>
2021-11-04 08:09:34 +0100

    express config

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello from the other side!");
+const express = require("express");
+
+// Create Server
+const app = express();
+
+// App PORT
+const PORT = process.env.PORT || 4000;
+
+// MAIN PAGE
+app.get("/", (req, res) => {
+  res.send("Hi world!");
+});
+
+// Starting APP
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`);
+});
